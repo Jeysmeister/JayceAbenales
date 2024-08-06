3503b29a2511328135749f34997cbc52c31b7d33
Jayce <john.abenales@eperformax.com>
2024-08-07 00:27:31 +0800

    responsive

--- app/common/SideNav.tsx
+++ app/common/SideNav.tsx
@@ -18,19 +18,34 @@ const logos = [
 
 const SideNav = () => {
   return (
-    <div className="absolute h-screen left-0 top-0 flex justify-center items-center px-2 flex-col gap-4">
-      {logos.map((logo, index) => {
-        return (
-          <Button
-            key={index}
-            variant={"ghost"}
-            className="w-fit h-fit rounded-full p-1 m-0 transition ease-in-out delay-150 hover:scale-110 cursor-pointer"
-          >
-            {logo.logo}
-          </Button>
-        );
-      })}
-    </div>
+    <>
+      <div className="absolute h-screen left-0 top-0 justify-center items-center px-2 flex-col gap-4 hidden sm:flex">
+        {logos.map((logo, index) => {
+          return (
+            <Button
+              key={index}
+              variant={"ghost"}
+              className="w-fit h-fit rounded-full p-1 m-0 transition ease-in-out delay-150 hover:scale-110 cursor-pointer"
+            >
+              {logo.logo}
+            </Button>
+          );
+        })}
+      </div>
+      <div className="absolute w-screen bottom-8 justify-center items-center px-2 gap-4 flex sm:hidden">
+        {logos.map((logo, index) => {
+          return (
+            <Button
+              key={index}
+              variant={"ghost"}
+              className="w-fit h-fit rounded-full p-1 m-0 transition ease-in-out delay-150 hover:scale-110 cursor-pointer"
+            >
+              {logo.logo}
+            </Button>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -23,21 +23,35 @@ const tools = [
 const Page = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <Card className="p-4 grid grid-cols-4 gap-4">
+      <Card className="p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-center">
         {tools.map((tool, index) => {
           return (
             <Card
               key={index}
-              className="p-2 flex gap-8 items-center w-[180px] h-[80px]"
+              className="p-2 flex gap-8 items-center w-[140px] sm:w-[180px] h-[40px] md:h-[80px] sm:h-[60px]"
             >
               <Image
-                className="dark:invert"
+                className="dark:invert hidden md:table"
                 src={tool.url}
                 alt={tool.name}
                 height={50}
                 width={50}
               />
-              <div className="w-full flex justify-center text-center">
+              <Image
+                className="dark:invert hidden sm:table md:hidden"
+                src={tool.url}
+                alt={tool.name}
+                height={25}
+                width={25}
+              />
+              <Image
+                className="dark:invert sm:hidden"
+                src={tool.url}
+                alt={tool.name}
+                height={20}
+                width={20}
+              />
+              <div className="w-full flex justify-center text-center text-xs sm:text-sm">
                 {tool.name}
               </div>
             </Card>
